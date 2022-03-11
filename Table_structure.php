8340b48777fedc7fe695999d1175d6278c3269d4
Kailash Meghwar <premanikailash97@gmail.com>
2022-03-11 20:22:55 +0500

    Table-Structure-In-HTML

---
+++ Table_structure.php
@@ -0,0 +1,42 @@
+
+<!DOCTYPE html>
+<html>
+            <head>
+                        <title>.::Table Strucure::.</title>
+           </head>
+<body>
+                   <h1 align="center" style="color:red">(((**Table Structure**)))</h1>
+                    <table border="2"  align="center " style="width:100% ; height:520px ;  background:skyblue"  >
+                    <tr >
+                     <td colspan="8"align="center">Header</td>
+                    </tr>
+                    
+                      <tr >
+                    <td rowspan="4" align="center">Side 1</td>
+                    <td colspan="6" align="center">Content 1</td>
+                     <td rowspan="4" align="center">Side 2</td>
+                    </tr>
+                    
+                     <tr>
+                    <td colspan="2" align="center">Img</td>
+                     <td colspan="2" align="center">Img</td>
+                     <td colspan="1" align="center">Img</td> 
+                     </tr>
+                     
+                    <tr>
+                    <td colspan="3" align="center">Content 2</td>
+                    <td colspan="3" align="center">Content 3</td>
+                    </tr>
+                     
+                    <tr >
+                    <td colspan="6" align="center">Content 4</td> 
+                    </tr>
+
+                    <tr >
+                    <td colspan="8" align="center">Footer</td>
+                    </tr>
+
+
+                    </table>
+</body>
+</html>
